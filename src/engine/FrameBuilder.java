8ba6fbb61600af33ef16e08fc0deff2a4576d22f
Tristan Lonsway <tblonswa@syr.edu>
2021-08-08 22:33:14 -0400

    Started adding z- buffer generator

---
+++ src/engine/FrameBuilder.java
@@ -0,0 +1,32 @@
+package engine;
+
+import java.awt.image.*;
+
+public class FrameBuilder {
+	int[][][] sBuff;
+	
+	public FrameBuilder(int[] screenDims) {
+		sBuff = new int[screenDims[0]][screenDims[1]][5];
+		
+	}
+	
+	public void renderTriangle(float[] p1, float[] p2, float[] p3) {
+		
+		
+		
+	}
+	
+	public BufferedImage getFrame() {
+		return null;
+	}
+	
+	//public void render3Poly(Polygon p) {
+		
+		
+	//}
+	
+	
+	
+	
+	
+}
