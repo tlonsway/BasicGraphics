ba58f4f17e597a29b7c8e35e6477b7722d6c5b48
Tristan Lonsway <tblonswa@syr.edu>
2021-11-17 20:18:25 -0500

    Fixed bug with waves

--- src/Game/Render/RenderWater.java
+++ src/Game/Render/RenderWater.java
@@ -114,8 +114,9 @@ public class RenderWater {
 					vertices[vertIn+1] = colT[1];
 					vertices[vertIn+2] = colT[2];
 					vertIn+=3;
-					vertices[vertIn] = (float)inc/10.0f;
-					inc+=inc%628;
+					vertices[vertIn] = (float)inc/100.0f;
+					System.out.println(vertices[vertIn]);
+					inc = (inc+1)%100;
 					vertIn++;
 				}
 			}
@@ -126,10 +127,12 @@ public class RenderWater {
 			glBindVertexArray(VAOt);
 			glBindBuffer(GL_ARRAY_BUFFER,VBOt);
 			glBufferData(GL_ARRAY_BUFFER, waterVerts, GL_STATIC_DRAW);
-			glVertexAttribPointer(0,3,GL_FLOAT,false,24,0l);
+			glVertexAttribPointer(0,3,GL_FLOAT,false,28,0l);
 			glEnableVertexAttribArray(0);
-			glVertexAttribPointer(1,3,GL_FLOAT,false,24,12l);
+			glVertexAttribPointer(1,3,GL_FLOAT,false,28,12l);
 			glEnableVertexAttribArray(1);
+			glVertexAttribPointer(3,1,GL_FLOAT,false,28,16l);
+			glEnableVertexAttribArray(3);
 			glBindBuffer(GL_ARRAY_BUFFER,0);
 			glBindVertexArray(0);
 			VAOHQ = VAOt;
@@ -155,8 +158,9 @@ public class RenderWater {
 					vertices[vertIn+1] = colT[1];
 					vertices[vertIn+2] = colT[2];
 					vertIn+=3;
-					vertices[vertIn] = (float)inc/10.0f;
-					inc+=inc%628;
+					vertices[vertIn] = (float)inc/100.0f;
+					inc = (inc+1)%100;
+					System.out.println(inc);
 					vertIn++;
 				}
 			}
@@ -171,8 +175,8 @@ public class RenderWater {
 			glEnableVertexAttribArray(0);
 			glVertexAttribPointer(1,3,GL_FLOAT,false,28,12l);
 			glEnableVertexAttribArray(1);
-			glVertexAttribPointer(2,3,GL_FLOAT,false,28,16l);
-			glEnableVertexAttribArray(2);
+			glVertexAttribPointer(3,1,GL_FLOAT,false,28,16l);
+			glEnableVertexAttribArray(3);
 			glBindBuffer(GL_ARRAY_BUFFER,0);
 			glBindVertexArray(0);
 			VAOLQ = VAOt;
