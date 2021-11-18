7926b3f88ce334f687604a286d3418944e14f266
Tristan Lonsway <tblonswa@syr.edu>
2021-11-17 22:36:26 -0500

    Added waves

--- src/Game/Render/RenderWater.java
+++ src/Game/Render/RenderWater.java
@@ -114,9 +114,9 @@ public class RenderWater {
 					vertices[vertIn+1] = colT[1];
 					vertices[vertIn+2] = colT[2];
 					vertIn+=3;
-					vertices[vertIn] = (float)inc/100.0f;
-					System.out.println(vertices[vertIn]);
-					inc = (inc+1)%100;
+					vertices[vertIn] = (float)inc/10.0f;
+					//System.out.println(vertices[vertIn]);
+					inc = (inc+1)%628;
 					vertIn++;
 				}
 			}
@@ -131,8 +131,8 @@ public class RenderWater {
 			glEnableVertexAttribArray(0);
 			glVertexAttribPointer(1,3,GL_FLOAT,false,28,12l);
 			glEnableVertexAttribArray(1);
-			glVertexAttribPointer(3,1,GL_FLOAT,false,28,16l);
-			glEnableVertexAttribArray(3);
+			glVertexAttribPointer(2,1,GL_FLOAT,false,28,24l);
+			glEnableVertexAttribArray(2);
 			glBindBuffer(GL_ARRAY_BUFFER,0);
 			glBindVertexArray(0);
 			VAOHQ = VAOt;
@@ -158,9 +158,9 @@ public class RenderWater {
 					vertices[vertIn+1] = colT[1];
 					vertices[vertIn+2] = colT[2];
 					vertIn+=3;
-					vertices[vertIn] = (float)inc/100.0f;
-					inc = (inc+1)%100;
-					System.out.println(inc);
+					vertices[vertIn] = (float)inc/10.0f;
+					inc = (inc+1)%628;
+					//System.out.println(inc);
 					vertIn++;
 				}
 			}
@@ -175,8 +175,8 @@ public class RenderWater {
 			glEnableVertexAttribArray(0);
 			glVertexAttribPointer(1,3,GL_FLOAT,false,28,12l);
 			glEnableVertexAttribArray(1);
-			glVertexAttribPointer(3,1,GL_FLOAT,false,28,16l);
-			glEnableVertexAttribArray(3);
+			glVertexAttribPointer(2,1,GL_FLOAT,false,28,24l);
+			glEnableVertexAttribArray(2);
 			glBindBuffer(GL_ARRAY_BUFFER,0);
 			glBindVertexArray(0);
 			VAOLQ = VAOt;
